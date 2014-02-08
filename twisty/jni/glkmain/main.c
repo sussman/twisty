e90d755ae060081d019aaa0d2161bb2847abf8f8
Jen Chen <jenchen0@gmail.com>
2014-02-07 19:31:14 -0600

    Add support for detecting supported game types by filepath and dynamically loading the appropriate interpreter.  Known supported types include .z* and .gblorb.
    KI: Opening a .gblorb file following any other file open will crash.

--- twisty/jni/glkmain/main.c
+++ twisty/jni/glkmain/main.c
@@ -1,38 +1,74 @@
+#include <stdio.h>
 #include <string.h>
 
 #include "glk.h"
 #include "glkstart.h"
 
-// TODO: Define a macro that creates the externs for the four glk names.
+#define GLK_TERP_DEF(NAME) \
+  extern glkunix_argumentlist_t glkunix_arguments_ ## NAME[]; \
+  extern int glkunix_startup_code_ ## NAME (glkunix_startup_t *data); \
+  extern void glk_main_ ## NAME (void); \
+  extern void glk_shutdown_ ## NAME (void); \
+  void run_using_ ##NAME() { \
+    int argidx = 0; \
+    while (glkunix_arguments_ ## NAME[argidx].argtype != glkunix_arg_End) { \
+      argidx++; \
+    } \
+    memcpy(glkunix_arguments, glkunix_arguments_ ## NAME, \
+        sizeof(glkunix_argumentlist_t) * (argidx + 1)); \
+    glk_main_ ## NAME(); \
+  }
+ 
+GLK_TERP_DEF(git);
+GLK_TERP_DEF(nitfol);
 
-extern glkunix_argumentlist_t glkunix_arguments_git[];
-extern int glkunix_startup_code_git(glkunix_startup_t *data);
-extern void glk_main_git(void);
-extern void glk_shutdown_git(void);
+typedef enum {UNKNOWN, NITFOL, GIT} supported_terps_t;
+supported_terps_t terp_to_use = UNKNOWN;
 
-extern glkunix_argumentlist_t glkunix_arguments_nitfol[];
-extern int glkunix_startup_code_nitfol(glkunix_startup_t *data);
-extern void glk_main_nitfol(void);
-extern void glk_shutdown_nitfol(void);
-
-glkunix_argumentlist_t glkunix_arguments[10];
+glkunix_argumentlist_t glkunix_arguments[100];
 
 int glkunix_startup_code(glkunix_startup_t *data) {
-  return glkunix_startup_code_git(data);
+  int found_zgame = 0;
+  int i, j;
+  for (i = 1; i < data->argc; ++i) {
+    const char* p = data->argv[i];
+
+    // Nitfol supports myriad flag commands, but expects
+    // at least one non-flag argv that contains the name
+    // of the file to open.
+    // Git expects exactly one argv that contains the name
+    // of the file to open.
+    if (p[0] != '-') {
+      for (j = 0; p[j] != '\0'; ++j) {
+        if (p[j] == '.' && p[j+1] == 'z') {
+          found_zgame = 1;
+          break;
+        }
+      }
+    }
+  }
+
+  if (found_zgame) {
+    terp_to_use = NITFOL;
+    return glkunix_startup_code_nitfol(data);
+  } else {
+    terp_to_use = GIT;
+    return glkunix_startup_code_git(data);
+  }
 }
 
 void glk_main() {
-  // TODO(jenchen): implement.
-  int argidx = 0;
-  while (glkunix_arguments_git[argidx].argtype != glkunix_arg_End) {
-    argidx++;
+  if (terp_to_use == GIT) {
+    run_using_git();
+  } else if (terp_to_use == NITFOL) {
+    run_using_nitfol();
   }
-  memcpy(glkunix_arguments, glkunix_arguments_git,
-      sizeof(glkunix_argumentlist_t) * (argidx + 1));
-  glk_main_git();
 }
 
 void glk_shutdown() {
-  // TODO(jenchen): implement.
-  glk_shutdown_git();
+  if (terp_to_use == GIT) {
+    glk_shutdown_git();
+  } else if (terp_to_use == NITFOL) {
+    glk_shutdown_nitfol();
+  }
 }
